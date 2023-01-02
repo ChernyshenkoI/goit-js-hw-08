6e1eecd23f234f25218a6e1d1a013e4dbcae8474
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-01-03 01:45:12 +0200

    feedback started

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
 
+contactFormEl = document.querySelector('.feedback-form');
+const userInfo = {};
+
+const fillContactFormFields = () =>{
+try {
+const userInfoFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (userInfoFromLS === null){
+ return
+ }
+for (const prop in userInfoFromLS){
+contactFormEl.elements[prop].value=userInfoFromLS[prop]
+ }
+} catch (err){
+console.error('err :>> ', err);
+ }
+} 
+
+fillContactFormFields();
+const onContactFieldChange = event =>{
+const {target} = event;
+const fieldValue = target.value;
+const fieldName = target.name; 
+userInfo[fieldName]=fieldValue;
+localStorage.setItem('feedback-form-state', JSON.stringify(userInfo));
+}
+
+const onContactFormSubmit= event =>{
+event.preventDefault();
+contactFormEl.reset();
+localStorage.removeItem('feedback-form-state');
+}
+
+contactFormEl.addEventListener('input', onContactFieldChange);
+contactFormEl.addEventListener('submit',onContactFormSubmit )
