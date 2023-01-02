2f168445d13caa06a5cdd217de93370ade3a0623
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-01-02 23:00:00 +0200

    1 task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryEL = document.querySelector(".gallery")
+
+function createMarkup(arr) {
+
+    const result = arr.map(({ preview, original, description }) => {
+
+        return `<li> <a class="gallery__item" 
+          href="${preview}">
+          <img class="gallery__image" 
+          src="${original}"  
+          alt=${description}" />
+ </a> </li>`
+
+    }).join("");
+
+    galleryEL.innerHTML = result;
+
+};
+
+createMarkup(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt", captionDelay: 250
+
+})
+
+console.log(SimpleLightbox);
