6409fa1a755ff701de700e69207b7b8946e363b0
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-01-04 22:02:48 +0200

    homework ready to check

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+
+
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    const STORAGE_KEY = 'player-current-time';
+    const currentPlayerTime = localStorage.getItem(STORAGE_KEY);
+
+
+
+    function onPlay({seconds}) {
+        localStorage.setItem(STORAGE_KEY, seconds)
+    };
+
+    
+    player.on('timeupdate', throttle(onPlay, 1000));
+    if (currentPlayerTime) {
+        player.setCurrentTime(currentPlayerTime);
+    }
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,41 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'feedback-form-state';
 contactFormEl = document.querySelector('.feedback-form');
-const userInfo = {};
+const userInfo = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
-const fillContactFormFields = () =>{
-try {
-const userInfoFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
-if (userInfoFromLS === null){
- return
- }
-for (const prop in userInfoFromLS){
-contactFormEl.elements[prop].value=userInfoFromLS[prop]
- }
-} catch (err){
-console.error('err :>> ', err);
- }
-} 
 
+const fillContactFormFields = () => {
+    try {
+        const userInfoFromLS = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        if (!userInfoFromLS) {
+            return
+        }
+        for (const prop in userInfoFromLS) {
+            contactFormEl.elements[prop].value = userInfoFromLS[prop]
+        }
+    } catch (err) {
+        console.error('err :>> ', err);
+    }
+}
 fillContactFormFields();
-const onContactFieldChange = event =>{
-const {target} = event;
-const fieldValue = target.value;
-const fieldName = target.name; 
-userInfo[fieldName]=fieldValue;
-localStorage.setItem('feedback-form-state', JSON.stringify(userInfo));
+
+const onContactFieldChange = event => {
+    const { target } = event;
+    const fieldValue = target.value;
+    const fieldName = target.name;
+    userInfo[fieldName] = fieldValue;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(userInfo));
 }
 
-const onContactFormSubmit= event =>{
-event.preventDefault();
-contactFormEl.reset();
-localStorage.removeItem('feedback-form-state');
+const onContactFormSubmit = event => {
+    event.preventDefault();
+    contactFormEl.reset();
+    console.log('userInfo :>> ', userInfo);
+    localStorage.removeItem(STORAGE_KEY);
+
 }
 
-contactFormEl.addEventListener('input', onContactFieldChange);
-contactFormEl.addEventListener('submit',onContactFormSubmit )
+
+contactFormEl.addEventListener('input', throttle(onContactFieldChange, 1000));
+contactFormEl.addEventListener('submit', onContactFormSubmit);
