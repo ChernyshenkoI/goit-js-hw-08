7380e607283937b7e414c2a055394a6800299485
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-01-06 20:41:00 +0200

    corrected error

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,7 @@
 import throttle from "lodash.throttle";
 
 const STORAGE_KEY = 'feedback-form-state';
-contactFormEl = document.querySelector('.feedback-form');
+const contactFormEl = document.querySelector('.feedback-form');
 const userInfo = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 
